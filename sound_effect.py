acc7218073ce56a210140ecb7ae94f9bd75b0c64
Roshan-Editor <nxtroshan03@gmail.com>
2026-07-23 18:56:17 +0530

    sound_effect.py

--- sound_effect.py
+++ sound_effect.py
@@ -1,19 +1,20 @@
 import random 
 import os 
-num=random.randint(1,3)
+
+# Range ko 1 se 4 kar diya hai taaki 4 options ho sakein
+num = random.randint(1, 4)
 print(num)
+
 os.system("clear")
 print("\033[1m\033[33m[\033[32m---Booting--\033[33m]\033[0m")
 
-
 # Condition 
-
 if num == 1:
     os.system("mpv Access-Granted.mp3")
-elif num==2:
+elif num == 2:
     os.system("mpv Jarvis2.mp3")
-else :
+elif num == 3:
     os.system("mpv Ram.mp3")
-    
- 
-###########################################
+else:
+    # Yahan apna naya music file ka naam likhein
+    os.system("mpv Mahadev.mp3")
