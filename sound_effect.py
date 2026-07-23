2570e47194e87d4a243088a2290874b0e95c47ce
Roshan-Editor <nxtroshan03@gmail.com>
2026-07-23 19:24:31 +0530

    sound_effect.py

--- sound_effect.py
+++ sound_effect.py
@@ -1,20 +1,19 @@
-import random 
+Import random 
 import os 
-
-# Range ko 1 se 4 kar diya hai taaki 4 options ho sakein
-num = random.randint(1,3)
+num=random.randint(1,3)
 print(num)
-
 os.system("clear")
 print("\033[1m\033[33m[\033[32m---Booting--\033[33m]\033[0m")
 
+
 # Condition 
+
 if num == 1:
-    os.system("mpv Access-Granted.mp3)
-elif num == 2:
+    os.system("mpv Access-Granted.mp3")
+elif num==2:
     os.system("mpv Jarvis2.mp3")
-elif num == 3:
+else :
     os.system("mpv Ram.mp3")
-
-
-#########################################################
+    
+ 
+###########################################
