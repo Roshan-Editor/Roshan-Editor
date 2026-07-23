a89212f297c6bde315c5bf467dc04d2cae89e1cd
Roshan-Editor <nxtroshan03@gmail.com>
2026-07-23 19:18:24 +0530

    sound_effect.py

--- sound_effect.py
+++ sound_effect.py
@@ -10,7 +10,7 @@ print("\033[1m\033[33m[\033[32m---Booting--\033[33m]\033[0m")
 
 # Condition 
 if num == 1:
-    os.system("mpv Mahadev.mp3")
+    os.system("mpv Access-Granted.mp3)
 elif num == 2:
     os.system("mpv Jarvis2.mp3")
 elif num == 3:
