41097714a0baa0b5f8df0ceabc3156ba7bdedab4
Roshan-Editor <nxtroshan03@gmail.com>
2026-07-23 18:37:16 +0530

    sound_effect.py

--- sound_effect.py
+++ sound_effect.py
@@ -1,6 +1,6 @@
 import random 
 import os 
-num=random.randint(1,4)
+num=random.randint(1,3)
 print(num)
 os.system("clear")
 print("\033[1m\033[33m[\033[32m---Booting--\033[33m]\033[0m")
@@ -12,9 +12,7 @@ if num == 1:
     os.system("mpv Access-Granted.mp3")
 elif num==2:
     os.system("mpv Jarvis2.mp3")
-else num==3:
+else :
     os.system("mpv JARVIS.mp3")
-    else :
-    os.system("mpv Ram.mp3")
  
 ###########################################
